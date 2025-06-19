6761eb0933b498701cc3589ffc70c43844a1204f
evan mavis <evanmavis3@gmail.com>
2025-06-19 12:54:13 -0400

    refactor: adjust positioning of Chevron icons in CornersFrame for improved layout

--- client/src/components/contentCornerFrame/CornersFrame.tsx
+++ client/src/components/contentCornerFrame/CornersFrame.tsx
@@ -3,25 +3,25 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 export function CornersFrame() {
   return (
     <>
-      <div className="absolute top-0 left-0 ">
+      <div className="absolute -top-1.5 -left-1.5 sm:top-0 sm:left-0">
         <ChevronLeft
           className="text-fuchsia-300"
           style={{ transform: "rotate(45deg)", fontSize: "0.8em" }}
         />
       </div>
-      <div className="absolute top-0 right-0 ">
+      <div className="absolute -top-1.5 -right-1.5 sm:top-0 sm:right-0">
         <ChevronRight
           className="text-fuchsia-300"
           style={{ transform: "rotate(-45deg)", fontSize: "0.8em" }}
         />
       </div>
-      <div className="absolute bottom-0 left-0 ">
+      <div className="absolute -bottom-1.5 -left-1.5 sm:bottom-0 sm:left-0">
         <ChevronLeft
           className="text-fuchsia-300"
           style={{ transform: "rotate(-45deg)", fontSize: "0.8em" }}
         />
       </div>
-      <div className="absolute bottom-0 right-0 ">
+      <div className="absolute -bottom-1.5 -right-1.5 sm:bottom-0 sm:right-0">
         <ChevronRight
           className="text-fuchsia-300"
           style={{ transform: "rotate(45deg)", fontSize: "0.8em" }}
