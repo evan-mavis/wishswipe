789a2b08f5880f521707b32c470336d8201cb836
evan mavis <evanmavis3@gmail.com>
2025-06-20 00:03:59 -0400

    feat: update launch configuration for debugging and add task definitions for development services

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  envDir: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	envDir: "./",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		open: true,
+	},
 });
