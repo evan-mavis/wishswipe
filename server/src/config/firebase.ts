ddeecc05f5a0920096acf0e7dfe4d7efe84fdea7
evan mavis <evanmavis3@gmail.com>
2025-06-19 23:00:17 -0400

    fix: update Firebase privateKey handling and add launch configuration for debugging

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,11 +1,11 @@
-import * as admin from 'firebase-admin';
-import dotenv from 'dotenv';
+import * as admin from "firebase-admin";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 } as admin.ServiceAccount;
 
