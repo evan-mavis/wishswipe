d08376d92531cddca07333e78d5ba90830e43b8a
evan mavis <evanmavis3@gmail.com>
2025-07-01 18:46:24 -0400

    add wishlist and wishlist items tables

---
+++ server/src/db/migrations/1751402732341_create-wishlist-tables.js
@@ -0,0 +1,68 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("wishlists", {
+    id: "uuid PRIMARY KEY",
+    user_id: {
+      type: "uuid",
+      notNull: true,
+      references: "users(id)",
+      onDelete: "cascade",
+    },
+    name: { type: "varchar(255)", notNull: true },
+    is_favorite: { type: "boolean", notNull: true, default: false },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+
+  pgm.createTable("wishlist_items", {
+    id: "id",
+    wishlist_id: {
+      type: "uuid",
+      notNull: true,
+      references: "wishlists(id)",
+      onDelete: "cascade",
+    },
+    ebay_item_id: { type: "varchar(64)", notNull: true },
+    title: { type: "varchar(255)" },
+    image_url: { type: "varchar(512)" },
+    item_web_url: { type: "varchar(512)" },
+    price: { type: "numeric" },
+    is_active: { type: "boolean", notNull: true, default: true },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("wishlist_items");
+  pgm.dropTable("wishlists");
+};
