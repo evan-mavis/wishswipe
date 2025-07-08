2781fc81a54508d8af260ed352867600f0f34308
evan mavis <evanmavis3@gmail.com>
2025-07-08 19:22:11 -0400

    fix: configure Redis client with maxRetriesPerRequest set to null for improved request handling

--- server/src/utils/redisClient.ts
+++ server/src/utils/redisClient.ts
@@ -1,7 +1,9 @@
 import { Redis } from "ioredis";
 import logger from "./logger.js";
 
-const redis = new Redis();
+const redis = new Redis({
+  maxRetriesPerRequest: null,
+});
 
 redis.on("error", (err: Error) => {
   logger.error("Redis error:", err);
