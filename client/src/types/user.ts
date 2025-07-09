d663ab6cf7f3a1e1c9c292bd911c88786a2f348b
evan mavis <evanmavis3@gmail.com>
2025-07-08 20:43:05 -0400

    refactor: update user response interface by renaming properties for consistency and clarity

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -7,14 +7,9 @@ export interface LoginOrCreateUserPayload {
 
 export interface LoginOrCreateUserResponse {
 	user: {
-		id: string;
-		firebase_uid: string;
 		email: string;
-		display_name?: string | null;
-		photo_url?: string | null;
-		created_at: string;
-		updated_at: string;
-		last_login?: string | null;
+		displayName?: string | null;
+		photoUrl?: string | null;
 	};
 	created: boolean;
 }
