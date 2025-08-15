12b2c03230be2f2455fd753ebf40a3af3199b8b0
evan mavis <evanmavis3@gmail.com>
2025-08-14 20:09:56 -0400

    remove uncessary sentence in swipe insights component

--- client/src/pages/swipeInsights/SwipeInsights.tsx
+++ client/src/pages/swipeInsights/SwipeInsights.tsx
@@ -56,8 +56,7 @@ export function SwipeInsights() {
 						<BarChartIcon className="text-xl" /> Swipe Insights
 					</h1>
 					<p className="text-muted-foreground">
-						Analytics from your swiping activity and wishlist data. Yes, I know
-						this is over-engineered!
+						Analytics from your swiping activity and wishlist data.
 					</p>
 				</div>
 				<div className="flex h-64 items-center justify-center">
